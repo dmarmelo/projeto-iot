47cee668b7e5938b1d7a155838d603df317df403
Daniel Marmelo <daniel.marmelo@hotmail.com>
2020-02-03 21:39:57 +0000

    Changed script for web files processing.

--- html/gzip-xxd.sh
+++ html/gzip-xxd.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
+# Ro minify the files http://minifycode.com/html-minifier/
+
 filename="$1"
 if [ -z "$filename" ]; then
 	echo "No filename provided!"
 else
-	# Can't remove white spaces from HTML
-	# http://minifycode.com/html-minifier/
-	#cat $filename | tr -d '\040\011\012\015' > "${filename}.min"
 	gzip -k "${filename}"
 	xxd -i "${filename}.gz" "${filename}.c"
 	rm "${filename}.gz"
